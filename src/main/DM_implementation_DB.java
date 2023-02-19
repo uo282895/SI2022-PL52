f46f3ffba73a4d9622773bb21bc70dbca5c182dc
uo282390 <uo282390@uniovi.es>
2023-02-19 21:10:18 +0100

    First creation of the controller Java Project.

---
+++ src/main/DM_implementation_DB.java
@@ -0,0 +1,8 @@
+package main;
+
+public class DM_implementation_DB {
+	public static void main(String[] args) {
+		System.out.println("Hello, from the first basic implementation of the"
+				+ " Data Base domain model");
+	}
+}
