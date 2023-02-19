6f523b5418d4759928de6a10a4bdf7168ae343f3
uo282390 <uo282390@uniovi.es>
2023-02-19 22:33:32 +0100

    Functions for execute SQLite queries and commands introduced

--- src/main/DM_implementation_DB.java
+++ src/main/DM_implementation_DB.java
@@ -17,18 +17,51 @@ public class DM_implementation_DB {
 		DefaultTableModel model;
 	    String url="jdbc:sqlite:" + System.getProperty("user.dir") + "/coursesdb";
 		Connection connect;
+		ResultSet result = null;
 	    
 		try{
 			// Trying to establish connection with the database called "coursesdb" in url variable
 			Class.forName("org.sqlite.JDBC");
 			connect = DriverManager.getConnection(url);
                   
-            if(connect != null)
+            if(connect != null) // Checking the connection
                 System.out.println("Conection Succesful!");
+            
+            PreparedStatement st = connect.prepareStatement("");
+            result = st.executeQuery();
                         
         }catch(Exception x){
         	System.err.println(x.getMessage().toString());
-                        
         }
 	}
+	
+	public boolean executeSQLcommand(String command, Connection connect) { // Function to execute table creation commands
+		try {
+			PreparedStatement st = connect.prepareStatement(command);
+			boolean result = st.execute();
+			if(result)
+				return true;
+			else
+				return false;
+		}catch(Exception x){
+        	System.err.println(x.getMessage().toString());
+        	return false;
+        }
+	}
+	
+	public ResultSet executeSQLquery(String command, Connection connect) { // Function to execute an SQL query
+		try {
+			ResultSet result;
+			PreparedStatement st = connect.prepareStatement(command);
+			result = st.executeQuery();
+			if(result != null)
+				return result;
+			else
+				return null;
+		}catch(Exception x){
+        	System.err.println(x.getMessage().toString());
+        	return null;
+        }
+	}
+	
 }
