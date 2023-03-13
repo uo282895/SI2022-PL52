fdfc6fed68ab82ab0b8b8a97203a6f5e8b5e7523
uo282895 <125304818+uo282895@users.noreply.github.com>
2023-03-13 01:06:16 +0100

    Add files via upload

---
+++ src/main/java/giis/demo/util/SwingMain.java
@@ -0,0 +1,96 @@
+package giis.demo.util;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import giis.demo.tkrun.*;
+
+/**
+ * Punto de entrada principal que incluye botones para la ejecucion de las pantallas 
+ * de las aplicaciones de ejemplo
+ * y acciones de inicializacion de la base de datos.
+ * No sigue MVC pues es solamente temporal para que durante el desarrollo se tenga posibilidad
+ * de realizar acciones de inicializacion
+ */
+public class SwingMain {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() { //NOSONAR codigo autogenerado
+			public void run() {
+				try {
+					SwingMain window = new SwingMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace(); //NOSONAR codigo autogenerado
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SwingMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Main");
+		frame.setBounds(0, 0, 287, 185);
+		frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		
+			
+		JButton btnInicializarBaseDeDatos = new JButton("Inicializar Base de Datos en Blanco");
+		btnInicializarBaseDeDatos.addActionListener(new ActionListener() { //NOSONAR codigo autogenerado
+			public void actionPerformed(ActionEvent e) {
+				Database db=new Database();
+				db.createDatabase(false);
+			}
+		});
+		
+		JButton btnPayments = new JButton("Register pending payments");
+		btnPayments.addActionListener(new ActionListener() { //NOSONAR codigo autogenerado
+			public void actionPerformed(ActionEvent e) {
+				PaymentsController controller=new PaymentsController(new PaymentsModel(), new PaymentsView());
+				controller.initControllerPayments();
+			}
+		});
+		frame.getContentPane().add(btnPayments);
+		frame.getContentPane().add(btnInicializarBaseDeDatos);
+			
+		JButton btnCargarDatosIniciales = new JButton("Cargar Datos Iniciales para Pruebas");
+		btnCargarDatosIniciales.addActionListener(new ActionListener() { //NOSONAR codigo autogenerado
+			public void actionPerformed(ActionEvent e) {
+				Database db=new Database();
+				db.createDatabase(false);
+				db.loadDatabase();
+			}
+		});
+		frame.getContentPane().add(btnCargarDatosIniciales);
+		
+		JButton btnCourses = new JButton("Consult courses");
+		btnCourses.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PaymentsController controller = new PaymentsController(new PaymentsModel(), new CoursesView());
+				controller.initControllerCourses();
+			}
+		});
+		frame.getContentPane().add(btnCourses);
+	}
+
+	public JFrame getFrame() { return this.frame; }
+	
+}
